d52536d6ae08a17f20557a779c80a6606b4eef8d
Andrew Taylor <rolyata47@gmail.com>
2022-05-29 21:47:01 -0500

    Adding colors and an ini file

--- wordprocessor.py
+++ wordprocessor.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import colorchooser
+from tkinter import font
+import configparser
+
+config = configparser.ConfigParser()
+config.read('wordprocessor.ini')
+fgcolor = config.get('DEFAULT', 'foreground_color', fallback='#ffffff')
+bgcolor = config.get('DEFAULT', 'background_color', fallback='#0099ff')
+fontfamily = config.get('DEFAULT', 'font_family', fallback='Courier New')
+fontsize = config.getint('DEFAULT', 'font_size', fallback=14)
 
 class WordProcessor(tk.Tk):
     def __init__(self):
@@ -9,8 +19,6 @@ class WordProcessor(tk.Tk):
         self.title("Word Processor")
         self.menubar = tk.Menu(self)
         self.filemenu = tk.Menu(self.menubar)
-        self.notebook = ttk.Notebook(self)
-        self.notebook.pack(expand=True, fill='both')
         self.filemenu.add_command(label='New', command=self.new_file)
         self.filemenu.add_command(label='Open', command=self.open_file)
         self.filemenu.add_command(label='Save', command=self.save_file)
@@ -18,19 +26,26 @@ class WordProcessor(tk.Tk):
         self.filemenu.add_command(label='Close tab', command=self.close_tab)
         self.filemenu.add_command(label='Exit', command=self.exit)
         self.menubar.add_cascade(label='File', menu=self.filemenu)
+        self.colormenu = tk.Menu(self.menubar)
+        self.colormenu.add_command(label='Select foreground', command=self.select_fgcolor)
+        self.colormenu.add_command(label='Select background', command=self.select_bgcolor)
+        self.menubar.add_cascade(label='Colors', menu=self.colormenu)
         self.config(menu=self.menubar)
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(expand=True, fill='both')
         self.notebook.bind('<<NotebookTabChanged>>', self.handle_tab_changed)
         self.refresh_menu_items()
+        self.font = font.Font(family=fontfamily, size=fontsize)
 
     def new_file(self):
-        text = tk.Text(self.notebook)
+        text = tk.Text(self.notebook, fg=fgcolor, bg=bgcolor, font=self.font)
         self.notebook.add(text, text='Untitled')
         self.notebook.select(self.notebook.tabs()[-1])
         self.refresh_menu_items()
 
     def open_file(self):
         filename = filedialog.askopenfilename()
-        text = tk.Text(self.notebook)
+        text = tk.Text(self.notebook, fg=fgcolor, bg=bgcolor, font=self.font)
         text.pack(expand=True, fill='both')
         self.notebook.add(text, text=filename)
         with open(filename, 'r') as f:
@@ -70,6 +85,18 @@ class WordProcessor(tk.Tk):
 
     def handle_tab_changed(self, *args):
         self.refresh_menu_items()
+
+    def select_fgcolor(self):
+        color = colorchooser.askcolor()
+        tabid = self.notebook.select()
+        text = self.notebook.nametowidget(tabid)
+        text.configure(fg=color[1])
+
+    def select_bgcolor(self):
+        color = colorchooser.askcolor()
+        tabid = self.notebook.select()
+        text = self.notebook.nametowidget(tabid)
+        text.configure(bg=color[1])
 
 if __name__ == '__main__':
     app = WordProcessor()
