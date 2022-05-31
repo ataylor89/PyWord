102d9f3d780baaa343e8321b441798825170ba85
Andrew Taylor <rolyata47@gmail.com>
2022-05-31 13:21:33 -0500

    Adding update functionality

--- wordprocessor.py
+++ wordprocessor.py
@@ -157,14 +157,34 @@ class WordProcessor(tk.Tk):
             tabid = self.notebook.select()
             filename = self.notebook.tab(tabid, "text")
             basename = os.path.basename(filename)
-            try:                                     
-                file_metadata = {'name': basename, 'parents': [drive_folder_id]}
-                media = MediaFileUpload(filename, mimetype='text/plain')
-                file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-                if file:
-                    print('Saved %s to the WordProcessor folder in Google Drive' %filename)
+            try:     
+                file_id = self.search_file_on_drive(basename)
+                if file_id:
+                    media = MediaFileUpload(filename, mimetype='text/plain')
+                    file = drive_service.files().update(fileId=file_id, media_body=media).execute()
+                    if file:
+                        print('Updated %s in the WordProcessor folder on Google Drive' %filename)
+                else:                                
+                    file_metadata = {'name': basename, 'parents': [drive_folder_id]}
+                    media = MediaFileUpload(filename, mimetype='text/plain')
+                    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+                    if file:
+                        print('Saved %s to the WordProcessor folder in Google Drive' %filename)
             except HttpError as error:
                 print(f'An error occurred: {error}')
+
+    def search_file_on_drive(self, filename):
+        if drive_enabled:
+            try:
+                queryString = "name='%s' and '%s' in parents" %(filename, drive_folder_id)
+                response = drive_service.files().list(q=queryString, spaces='drive', fields='files(id, name)').execute()
+                for file in response.get('files', []):
+                    if file.get('name') == filename:
+                        print('Found file %s (file ID %s)' %(filename, file.get('id')))
+                        return file.get('id')
+            except HttpError as error:
+                print(f'An error occurred: {error}')
+        return None
  
 if __name__ == '__main__':
     app = WordProcessor()
